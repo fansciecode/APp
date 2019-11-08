0a310d7c64a87d965526df188c4a3d276a44be8d
jhon_stark <jhonsnowgt@protonmail.com>
2019-11-08 21:11:28 +0530

    db changes

--- config/database.js
+++ config/database.js
@@ -4,3 +4,12 @@ const mongoDB = 'mongodb://localhost:27017/IBC';
 mongoose.connect(mongoDB,{ useUnifiedTopology: true , useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://admin:<password>@newapp-1aphy.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
--- routes/query.js
+++ routes/query.js
@@ -8,3 +8,13 @@ router.get('/location/:location', QueryController.EventByLocation);
 router.get('/category/:category',QueryController.EventByCategory);
 // router.get('/location&category')
 module.exports= router;
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://admin:<password>@newapp-1aphy.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
